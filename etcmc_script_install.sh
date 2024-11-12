f87d58ce9c14ee841a122db03f58f1997a966a8f
dark_knight0257 <project.aod+github@gmail.com>
2024-11-12 13:38:28 +0200

    Added absolute paths where applicable. Updated permissions (chmod 755) to make them secure. Streamlined etcmc.service creation for readability.

--- etcmc_script_install.sh
+++ etcmc_script_install.sh
@@ -4,80 +4,102 @@ echo 'Updating Linux...'
 apt update 
 apt dist-upgrade -y 
 clear
+
 echo 'Initializing ETCMC Linux Installation...'
 sleep 5
-mkdir etcmc 
-wget https://github.com/Nowalski/ETCMC_Software/releases/download/Setup%2FWindows/ETCMC_Linux.zip 
+mkdir -p /root/etcmc
+wget https://github.com/Nowalski/ETCMC_Software/releases/download/Setup%2FWindows/ETCMC_Linux.zip -O /root/ETCMC_Linux.zip
 clear
+
 echo 'Installing ETCMC Linux...'
 sleep 5
 apt install unzip python3 python3-pip screen -y 
-unzip ETCMC_Linux.zip -d etcmc 
-chmod -R 777 etcmc/ 
-cd etcmc/ 
-pip install -r requirements.txt 
+unzip /root/ETCMC_Linux.zip -d /root/etcmc
+chmod -R 755 /root/etcmc/
+cd /root/etcmc
+pip install -r requirements.txt
 chmod +x Linux.py ETCMC_GETH.py updater.py geth
 clear
 
 echo 'Creating ETCMC Linux Scripts...'
 sleep 5
 
-cat << EOF > ~/etcmc/start.sh
+cat << EOF > /root/etcmc/start.sh
 #!/bin/sh
-#creating etcmc screen session as detached
-screen -dmS etcmc
+# Creating etcmc screen session as detached
+/usr/bin/screen -dmS etcmc
 
-#starting etcmc node on port 5000 and exit screen session immediately when stopped
-screen -S etcmc -X stuff "/usr/bin/python3 /root/etcmc/Linux.py start --port 5000\n" 1>/dev/null 2>&1
-screen -S etcmc -X quit 1>/dev/null 2>&1
+# Starting etcmc node on port 5000 and exit screen session immediately when stopped
+/usr/bin/screen -S etcmc -X stuff "/usr/bin/python3 /root/etcmc/Linux.py start --port 5000\n" 1>/dev/null 2>&1
+/usr/bin/screen -S etcmc -X quit 1>/dev/null 2>&1
 EOF
-chmod +x start.sh
+chmod +x /root/etcmc/start.sh
 
-cat << EOF > ~/etcmc/stop.sh
+cat << EOF > /root/etcmc/stop.sh
 #!/bin/sh
-#stopping etcmc node
-python3 Linux.py stop
+# Stopping etcmc node
+/usr/bin/python3 /root/etcmc/Linux.py stop
 EOF
-chmod +x stop.sh
+chmod +x /root/etcmc/stop.sh
 
-cat << EOF > ~/etcmc/poweroff.sh
+cat << EOF > /root/etcmc/poweroff.sh
 #!/bin/sh
-#stopping etcmc node
-~/etcmc/stop.sh
+# Stopping etcmc node
+/root/etcmc/stop.sh
 
-#powering down machine
-poweroff
+# Powering down machine
+/sbin/poweroff
 EOF
-chmod +x poweroff.sh
+chmod +x /root/etcmc/poweroff.sh
 
-cat << EOF > ~/etcmc/update.sh
+cat << EOF > /root/etcmc/update.sh
 #!/bin/sh
-#stopping etcmc node
-~/etcmc/stop.sh
+# Stopping etcmc node
+/root/etcmc/stop.sh
 
-#updating etcmc node
-python3 Linux.py update
+# Updating etcmc node
+/usr/bin/python3 /root/etcmc/Linux.py update
 
-echo 'ETCMC Updated. Starting ETCMC Node now...' 
+echo 'ETCMC Updated. Starting ETCMC Node now...'
 sleep 5
 
-#starting etcmc node
-~/etcmc/start.sh
+# Starting etcmc node
+/root/etcmc/start.sh
 EOF
-chmod +x update.sh
+chmod +x /root/etcmc/update.sh
 
 clear
 echo 'Creating ETCMC Service Script...'
 sleep 5
-touch /etc/systemd/system/etcmc.service 
-{ echo '[Unit]' ; echo 'Description=ETCMC' ; echo 'After=network.target' ; echo '' ; echo '[Service]' ; echo 'User=root' ; echo 'Group=root' ; echo 'ExecStartPre=/bin/sleep 20' ; echo 'ExecStart=/root/etcmc/start.sh' ; echo 'WorkingDirectory=/root/etcmc' ; echo 'StandardOutput=append:/var/log/etcmcscript.log' ; echo 'StandardError=append:/var/log/etcmcscript.log' ; echo 'Environment=PATH=/usr/local/bin:/usr/bin:/bin' ; echo 'RemainAfterExit=true' ; echo '' ; echo '[Install]' ; echo 'WantedBy=multi-user.target' ; }  | tee -a /etc/systemd/system/etcmc.service 
-systemctl daemon-reload 
+
+cat << EOF > /etc/systemd/system/etcmc.service
+[Unit]
+Description=ETCMC
+After=network.target
+
+[Service]
+User=root
+Group=root
+ExecStartPre=/bin/sleep 20
+ExecStart=/root/etcmc/start.sh
+WorkingDirectory=/root/etcmc
+StandardOutput=append:/var/log/etcmcscript.log
+StandardError=append:/var/log/etcmcscript.log
+Environment=PATH=/usr/local/bin:/usr/bin:/bin
+RemainAfterExit=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
 systemctl enable etcmc.service
 clear
+
 echo 'Installation complete... Rebooting ETCMC Node in 20 seconds...'
-echo 'Once the Reboot is complete, the ETCMC Node will start automatically. You can then access the Node Web UI at:'
+echo 'Once the reboot is complete, the ETCMC Node will start automatically. Please wait until the reboot is finished, then access the Node Web UI at:'
 my_ip=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
-echo 'http://'$my_ip':5000'
+echo "http://$my_ip:5000"
 sleep 20
 echo 'Rebooting...'
-reboot
+/sbin/reboot
