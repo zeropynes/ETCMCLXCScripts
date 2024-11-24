4be351b9e06d0cf34a2e6a5bcc7ef555e9ff46a2
dark_knight0257 <project.aod+github@gmail.com>
2024-11-24 12:01:15 +0200

    Flask Fix

--- etcmc_script_install.sh
+++ etcmc_script_install.sh
@@ -18,6 +18,7 @@ unzip /root/ETCMC_Linux.zip -d /root/etcmc
 chmod -R 755 /root/etcmc/
 cd /root/etcmc
 pip install -r requirements.txt
+pip3 install flask --break-system-packages --ignore-installed
 chmod +x Linux.py ETCMC_GETH.py updater.py geth
 clear
 
