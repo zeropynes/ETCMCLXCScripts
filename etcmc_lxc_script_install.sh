4536d21be1d7aca233d87ae581e0a4874319dde4
dark_knight0257 <project.aod+github@gmail.com>
2025-01-08 17:36:26 +0200

    Update

--- etcmc_lxc_script_install.sh
+++ etcmc_lxc_script_install.sh
@@ -5,6 +5,29 @@ apt update
 apt dist-upgrade -y 
 clear
 
+echo -e "\033[1;31m" # Red text color
+cat << "EOF"
+    ____________________  _________             
+   / ____/_  __/ ____/  |/  / ____/             
+  / __/   / / / /   / /|_/ / /                  
+ / /___  / / / /___/ /  / / /___                
+/_____/ /_/ _\____/_/  /_/\____/  __            
+   / /  | |/ / ____/  / ___/___  / /___  ______ 
+  / /   |   / /       \__ \/ _ \/ __/ / / / __ \
+ / /___/   / /___    ___/ /  __/ /_/ /_/ / /_/ /
+/_____/_/|_\____/   /____/\___/\__/\__,_/ .___/ 
+                                       /_/      
+EOF
+echo -e "\033[1;36m" # Cyan text color
+cat << "EOF"
+      CREATED BY dark_knight
+
+EOF
+echo -e "\033[0m" # Reset text color
+echo "PS: If you would like to donate to support ongoing development, send ETC/ETCPOW to our wallet:"
+echo "0xA24BC49b794b3bEc609aD72cD7017FFB6e784E2C"
+sleep 30
+
 echo 'Initializing ETCMC Linux Installation...'
 sleep 5
 mkdir -p /root/etcmc
@@ -13,7 +36,7 @@ clear
 
 echo 'Installing ETCMC Linux...'
 sleep 5
-apt install unzip python3 python3-pip screen curl jq -y 
+apt install unzip python3 python3-pip screen curl jq  -y 
 unzip /root/ETCMC_Linux.zip -d /root/etcmc
 chmod -R 775 /root/etcmc/
 cd /root/etcmc
