23a2bf8b485e18db55d70b380f87efc533613ba6
dark_knight0257 <project.aod+github@gmail.com>
2025-01-08 18:02:53 +0200

    Update

--- etcmc_lxc_script_install.sh
+++ etcmc_lxc_script_install.sh
@@ -178,5 +178,30 @@ echo 'Please wait until the reboot is finished, then access the Node Web UI at:'
 my_ip=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
 echo "http://$my_ip:5000"
 sleep 20
-echo 'Rebooting...'
-/sbin/reboot
+
+clear
+echo 'Please Reboot Manually...'
+echo "\n"
+#/sbin/reboot
+
+echo -e "\033[1;36m" # Cyan text color
+cat << "EOF"
+   ____                _           _   ____              
+  / ___|_ __ ___  __ _| |_ ___  __| | | __ ) _   _       
+ | |   | '__/ _ \/ _` | __/ _ \/ _` | |  _ \| | | |      
+ | |___| | |  __/ (_| | ||  __/ (_| | | |_) | |_| |      
+  \____|_|  \___|\__,_|\__\___|\__,_| |____/ \__, |      
+                                             |___/       
+      _            _        _          _       _     _   
+   __| | __ _ _ __| | __   | | ___ __ (_) __ _| |__ | |_ 
+  / _` |/ _` | '__| |/ /   | |/ / '_ \| |/ _` | '_ \| __|
+ | (_| | (_| | |  |   <    |   <| | | | | (_| | | | | |_ 
+  \__,_|\__,_|_|  |_|\_\___|_|\_\_| |_|_|\__, |_| |_|\__|
+                      |_____|            |___/           
+EOF
+
+echo "\n"
+echo -e "\033[1;33m" # Cyan text color
+echo "Please Donate ETC/ETCPOW to our wallet:"
+echo "0xA24BC49b794b3bEc609aD72cD7017FFB6e784E2C"
+echo -e "\033[0m" # Reset text color
