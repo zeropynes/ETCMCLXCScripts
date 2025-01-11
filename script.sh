fbd4142315c3b41c5fc0bba65da4da527296d6a8
dark_knight0257 <project.aod+github@gmail.com>
2025-01-11 14:00:02 +0200

    Update Credits

--- script.sh
+++ script.sh
@@ -127,3 +127,25 @@ copy_config "$DEST_DIR"
 
 log_message "Script completed successfully."
 echo "Thank you for using this tool!"
+echo "\n"
+echo -e "\033[1;36m" # Cyan text color
+cat << "EOF"
+   ____                _           _   ____              
+  / ___|_ __ ___  __ _| |_ ___  __| | | __ ) _   _       
+ | |   | '__/ _ \/ _` | __/ _ \/ _` | |  _ \| | | |      
+ | |___| | |  __/ (_| | ||  __/ (_| | | |_) | |_| |      
+  \____|_|  \___|\__,_|\__\___|\__,_| |____/ \__, |      
+                                             |___/       
+      _            _        _          _       _     _   
+   __| | __ _ _ __| | __   | | ___ __ (_) __ _| |__ | |_ 
+  / _` |/ _` | '__| |/ /   | |/ / '_ \| |/ _` | '_ \| __|
+ | (_| | (_| | |  |   <    |   <| | | | | (_| | | | | |_ 
+  \__,_|\__,_|_|  |_|\_\___|_|\_\_| |_|_|\__, |_| |_|\__|
+                      |_____|            |___/           
+EOF
+
+echo "\n"
+echo -e "\033[1;33m" # Cyan text color
+echo "Please Donate ETC/ETCPOW to our wallet:"
+echo "0xA24BC49b794b3bEc609aD72cD7017FFB6e784E2C"
+echo -e "\033[0m" # Reset text color
